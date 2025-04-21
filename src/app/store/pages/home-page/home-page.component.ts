eb52e2b851c68cd36ee44b9448b99ef03d0e2ca9
rmkw <creditozona1@gmail.com>
2025-04-21 11:26:30 -0600

    full style

--- src/app/store/pages/home-page/home-page.component.ts
+++ src/app/store/pages/home-page/home-page.component.ts
@@ -4,22 +4,20 @@ import { UserService } from '../../../products/services/user.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
   // _userService = inject(UserService);
-
   // UsersResource = rxResource({
   //   request: () => ({}),
   //   loader: ({ request }) => {
   //     return this._userService.getUsers();
   //   },
   // });
-
-
 }
