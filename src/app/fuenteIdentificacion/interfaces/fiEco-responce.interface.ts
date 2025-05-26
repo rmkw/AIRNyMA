b285a77becbf7635cfbec9abeb2b55e3a06861e8
rmkw <creditozona1@gmail.com>
2025-05-26 11:57:22 -0600

    se agrego el campo de variables capturadas a la tabla de las fuentes

--- src/app/fuenteIdentificacion/interfaces/fiEco-responce.interface.ts
+++ src/app/fuenteIdentificacion/interfaces/fiEco-responce.interface.ts
@@ -1,12 +1,13 @@
 export interface FiEcoResponce {
-  idFuente:                 number;
-  idPp?:                     string;
-  fuente:                   string;
-  linkFuente:               string;
-  linkAccesoFuente?:        string | null;
-  anioEvento:               string;
-  comentario:               string;
-  isactive?:                boolean | null;
-  responsableRegister?:     number;
+  idFuente: number;
+  idPp?: string;
+  fuente: string;
+  linkFuente: string;
+  linkAccesoFuente?: string | null;
+  anioEvento: string;
+  comentario: string;
+  isactive?: boolean | null;
+  responsableRegister?: number;
   responsableActualizacion: number | null;
+  totalVariables?: number;
 }
