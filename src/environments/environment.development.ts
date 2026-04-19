f94c1cbd619279f1aa3fcc9987993546e0a23e72
rmkw <creditozona1@gmail.com>
2026-04-18 22:09:53 -0600

    chore(config): update environment and ts config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,3 @@
 export const environment = {
- baseUrl: 'http://10.200.130.27:8090/armo/api',
+  baseUrl: 'http://localhost:3000/api',
 };
-
-//? HOMEOFF
-//export const environment = {
-//   baseUrl: 'http://localhost:3000/api',
-// };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
-export const environment = {
-  baseUrl: 'http://10.200.130.27:8090/armo/api',
-};
-//? HOMEOFF
 // export const environment = {
-//   baseUrl: 'http://localhost:3000/api',
+//   baseUrl: 'http://10.200.130.27:8090/armo/api',
 // };
+//? HOMEOFF
+export const environment = {
+  baseUrl: 'http://localhost:3000/api',
+};
